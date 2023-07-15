ca5f1fd9ba5a65f75156c0174a1c43de6e9f7a92
TaeHye0n <thk001229@gmail.com>
2023-07-15 17:53:45 +0900

    Request-Param-Map

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -74,6 +75,15 @@ public class RequestParamController {
             //defaultValue가 들어가면 required가 의미가 없음,
             //빈 문자의 경우에도 기본 값이 적용됨
         log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
 
         return "ok";
     }
